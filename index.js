4f55cd02ea9d4c30050ff55008f2928295a12448
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-05-14 18:14:10 -0400

    right arrow button now switches to the next photo in the project photo slideshow

---
+++ index.js
@@ -0,0 +1,90 @@
+$(function() {
+    /*
+        Assigns a unique ID to each project on the page
+    */
+    function assignProjectIDs() {
+        let projects = $("section.project");
+        for(project of projects) {
+            $(project).attr("data-project-id", cuid());
+        }
+    }
+
+    /*
+        Returns true if the currently-displayed photo is the last photo in its 
+        project photo slideshow
+    */
+    function onLastPhoto(projectID) {
+        let currentProject = $(`section.project[data-project-id="${projectID}"]`);
+        let currentPhoto = currentProject.find(`.project-photo.js-current-photo`);
+        
+        let nextPhoto = currentPhoto.next(".project-photo");
+        if(nextPhoto.length === 0) { //i.e. there aren't any next photos
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /*
+        Hides from the page the currently displayed photo, in the project with 
+        the provided projectID
+    */
+    function hideCurrentProjectPhoto(projectID) {
+        let currentProject = $(`section.project[data-project-id="${projectID}"]`);
+        let currentPhoto = currentProject.find(`.project-photo.js-current-photo`);
+
+        currentPhoto.removeClass("js-current-photo");
+        currentPhoto.hide();
+    }
+
+    /*
+        Shows on the page the project photo with the provided photo ID, in the project with the provided projectID
+    */
+    function showProjectPhoto(projectID, photoID) {
+        let project = $(`section.project[data-project-id="${projectID}"]`);
+        let photoToShow = project.find(`.project-photo[data-photo-id="${photoID}"]`);
+
+        /* 
+        Checks if there are photos with that ID to show (e.g. there might not be a "next" photo after the current one)
+        */
+        if(photoToShow.length > 0) {
+            photoToShow.addClass("js-current-photo");
+            photoToShow.show();
+        }
+    }
+
+    /*
+        Handles switching to the next photo when the right arrow button is 
+        clicked
+    */
+    function handleNextImage() {
+        $(".right-arrow-button").click(event => {
+            let projectID = $(event.currentTarget).parents("section.project").data("project-id");
+            let currentPhoto = $(event.currentTarget).parents(".project-slideshow").find(".project-photo.js-current-photo");
+            let currentPhotoID = currentPhoto.data("photo-id");
+
+            let notOnLastPhoto = !onLastPhoto(projectID);
+            if(notOnLastPhoto) {
+                hideCurrentProjectPhoto(projectID);
+
+                let nextPhotoID = currentPhotoID + 1;
+                showProjectPhoto(projectID, nextPhotoID);
+            }
+        });
+    }
+
+    /*
+        Handles switching to the previous image when the left arrow button is
+        clicked
+    */
+    function handlePrevImage() {
+        $(".left-arrow-button").click(event => {
+            alert("Previous button clicked");
+        });
+    }
+
+    assignProjectIDs();
+    handleNextImage();
+    handlePrevImage();
+});
